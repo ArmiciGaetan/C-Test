587f4264258210253893df6fe34f81a68a4a3779
ArmiciGaetan <gaetanarmici@gmail.com>
2015-10-01 16:31:59 +0200

    for test

---
+++ Labo2/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2
+{
+    public abstract class Person 
+    {
+
+        public String Name { get; set; }
+        public String LastName { get; set; }
+
+        public Person(String name,String lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + LastName;
+        }
+    }
+}
